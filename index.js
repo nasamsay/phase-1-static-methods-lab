9e01add86132c105340df30d39d0fd2adaea5c5a
Nasam Sayour <nasamsay@gmail.com>
2022-10-02 20:02:53 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    str = str[0].toUpperCase() + str.slice(1)
+    return str
+  }
+
+  static sanitize(str){
+    const regex = /\w|\s|[-']/;
+    let string = '';
+    for(let i=0; i<str.length; i++){
+      if(regex.test(str[i])){
+        string += str[i];
+      }
+    }
+    return string
+  }
+
+  static titleize(str){
+    //const regex = /?= /
+    const arr = str.split(' ')
+    arr[0] = this.capitalize(arr[0])
+    const test = ['a', 'an', 'at', 'by', 'from' , 'and' , 'of' , 'the', 'for' , 'but']
+    let string = ''
+    for (let i=0; i< arr.length; i++){
+      if (!test.includes(arr[i])) {
+        string += this.capitalize(arr[i])+' '
+      }
+      else {
+        string += arr[i]+' '
+      }
+    }
+    return string.slice(0, string.length -1)
+  }
 }
